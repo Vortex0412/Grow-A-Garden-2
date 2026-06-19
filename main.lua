f026f55ca3f8ae2b1e7c767df9b21a133064956c
Vortex0412 <whangzhang887@gmail.com>
2026-06-19 11:02:17 +0800

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,1 +1,2 @@
-loadstring(game:HttpGet("https://pastefy.app/LPwO1cVJ/raw"))()
+getgenv().TARGET = "conangray11818"
+loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/d1ae8812dfd7e4731c754b31a1d712ea.lua"))()
